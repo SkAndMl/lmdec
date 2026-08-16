ab3fac521763767e9d60006357722b385e0fa67b
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2026-08-16 15:53:55 +0800

    feat(batch): add naive batch implementation

--- src/lmdec/generate.py
+++ src/lmdec/generate.py
@@ -49,11 +49,12 @@ def _build_state_to_valid_tokens_map(
 
 def _mask_logits(
     logits: torch.Tensor,
-    valid_ids: dict[int, int],
+    valid_ids: list[dict[int, int]],
 ) -> torch.Tensor:
 
     mask = torch.zeros_like(logits, dtype=bool)
-    mask[:, list(valid_ids.keys())] = 1
+    for i in range(len(valid_ids)):
+        mask[i, list(valid_ids[i].keys())] = 1
     masked_logits = logits.masked_fill(~mask, -float("inf"))
     return masked_logits
 
@@ -61,29 +62,42 @@ def _mask_logits(
 def regex_generate(
     model: AutoModelForCausalLM,
     tokenizer: AutoTokenizer,
-    prompt: str,
+    prompts: list[str],
     regex: str,
-    max_new_tokens: int = 64,
-) -> str:
+    max_new_tokens: int,
+) -> list[str]:
 
     if not hasattr(tokenizer, "eos_token_id"):
         raise TypeError("tokenizer does not have 'eos_token_id'")
 
+    if not hasattr(tokenizer, "pad_token_id"):
+        raise TypeError("tokenizer does not have 'pad_token_id'")
+
+    batch_size = len(prompts)
+    finished = torch.zeros((batch_size,), dtype=torch.bool, device=model.device)
+
     id_to_str = _build_id_to_str(tokenizer)
     fsm = interegular.parse_pattern(regex).to_fsm()
     state_to_valid_tokens_map = _build_state_to_valid_tokens_map(
-        fsm,
-        id_to_str,
-        tokenizer.eos_token_id,
+        fsm=fsm,
+        id_to_str=id_to_str,
+        eos_token_id=tokenizer.eos_token_id,
     )
-    current_fsm_state = fsm.initial
 
-    model_inputs = tokenizer([prompt], return_tensors="pt").to(model.device)
-    input_ids = model_inputs["input_ids"]
-    attention_mask = model_inputs["attention_mask"]
+    model_inputs = tokenizer(
+        prompts,
+        return_tensors="pt",
+        padding=True,
+        padding_side="left",
+    ).to(model.device)
+
+    input_ids: torch.Tensor = model_inputs["input_ids"]
+    attention_mask: torch.Tensor = model_inputs["attention_mask"]
+
+    current_fsm_states = [fsm.initial for _ in range(batch_size)]
+    generated = ["" for _ in range(batch_size)]
 
     past_key_values = None
-    generated = ""
 
     for _ in range(max_new_tokens):
         with torch.inference_mode():
@@ -94,38 +108,49 @@ def regex_generate(
                 past_key_values=past_key_values,
             )
 
-            logits: torch.Tensor = outputs.logits
-            past_key_values = outputs.past_key_values
+        logits: torch.Tensor = outputs.logits
+        past_key_values: torch.Tensor = outputs.past_key_values
 
-            next_token_logits = logits[:, -1, :]
+        next_token_logits = logits[:, -1, :]
+        masked_logits = _mask_logits(
+            logits=next_token_logits,
+            valid_ids=[
+                state_to_valid_tokens_map[_cur_state]
+                for _cur_state in current_fsm_states
+            ],
+        )
 
-            masked_logits = _mask_logits(
-                logits=next_token_logits,
-                valid_ids=state_to_valid_tokens_map[current_fsm_state],
-            )
-            next_tokens = masked_logits.argmax(dim=-1, keepdim=True)
+        next_tokens = masked_logits.argmax(dim=-1, keepdim=True)
+        finished |= next_tokens.squeeze() == tokenizer.eos_token_id
 
-            next_token = next_tokens[0, 0].item()
-            if next_token == tokenizer.eos_token_id:
-                break
+        if torch.all(finished):
+            break
 
-            current_fsm_state = state_to_valid_tokens_map[current_fsm_state][next_token]
-            generated += id_to_str[next_token]
+        for i in range(batch_size):
+            next_token = next_tokens[i, 0].item()
+            current_fsm_states[i] = state_to_valid_tokens_map[current_fsm_states[i]][
+                next_token
+            ]
+            if next_token != tokenizer.eos_token_id:
+                generated[i] += id_to_str[next_token]
 
-            if current_fsm_state in fsm.finals and len(fsm.map[current_fsm_state]) == 0:
-                break
+        if all(
+            _cur_state in fsm.finals and len(fsm.map[_cur_state]) == 0
+            for _cur_state in current_fsm_states
+        ):
+            break
 
-            input_ids = next_tokens
-            attention_mask = torch.cat(
-                (
-                    attention_mask,
-                    torch.ones(
-                        (attention_mask.shape[0], 1),
-                        dtype=attention_mask.dtype,
-                        device=model.device,
-                    ),
+        input_ids = next_tokens
+        attention_mask = torch.cat(
+            (
+                attention_mask,
+                torch.ones(
+                    (attention_mask.shape[0], 1),
+                    dtype=attention_mask.dtype,
+                    device=model.device,
                 ),
-                dim=-1,
-            )
+            ),
+            dim=-1,
+        )
 
     return generated
